8ac8a8c93ac6b1b37bf30e95d8be51124c123d70
HashedDan <georgedanielmangum@gmail.com>
2018-03-21 11:13:32 -0500

    folder restructure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
