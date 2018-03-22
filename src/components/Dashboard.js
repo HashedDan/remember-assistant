f11b9314c2ca50d15fd9a7a489d7b2c02bb8cccb
HashedDan <georgedanielmangum@gmail.com>
2018-03-21 21:47:12 -0500

    multiple updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <p>This is the dashboard.</p>
+        <Button>Click Here</Button>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment, Input } from 'semantic-ui-react'
 import './Home/Home.css';
 import Dashboard from './Dashboard';
 import Login from './Login';
 
 class Home extends Component {
+  
   render() {
     return (
       <div>
-        <h1>Welcome to Re-Member Assistant!</h1>
-        <h3>Please login below...</h3>
-      <Router>
-        <div>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/login" component={Login} />
-        </div>
-      </Router>
+        <Menu fixed='top' inverted>
+          <Container>
+            <Menu.Item as='a' header>
+              <Image
+                size='mini'
+                src='./feather.png'
+                style={{ marginRight: '1.5em' }}
+              />
+              Re-Member AB
+        </Menu.Item>
+            <Menu.Item as='a'>Home</Menu.Item>
+
+            <Dropdown item simple text='Dropdown'>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Header Item</Dropdown.Header>
+                <Dropdown.Item>
+                  <i className='dropdown icon' />
+                  <span className='text'>Submenu</span>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Menu.Menu position='right'>
+              <Menu.Item>
+                <h4>Daniel Mangum</h4>
+              </Menu.Item>
+              <Menu.Item name='logout' />
+            </Menu.Menu>
+          </Container>
+        </Menu>
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>Semantic UI React Fixed Template</Header>
+          <p>This is a basic fixed menu template using fixed size containers.</p>
+          <p>A text container is used for the main container, which is useful for single column layouts.</p>
+          <h1>Welcome to Re-Member Assistant!</h1>
+          <h3>Please login below...</h3>
+          <Router>
+            <div>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/login" component={Login} />
+            </div>
+          </Router>
+        </Container>
+
       </div>
     );
   }
